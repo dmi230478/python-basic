f5cc41521f601c79a67d697fc066990ff903bd96
dmi230478 <dmi230478@yandex.ru>
2022-08-06 20:36:11 +0300

    add odd and even

--- homework_01/main.py
+++ homework_01/main.py
@@ -17,7 +17,16 @@ def power_numbers(* numbers):
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
+def is_prime(num):
+    prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
+    i = 5;
+    d = 2;
 
+    while prime and i * i <= num:
+        prime = num % i != 0
+        i += d
+        d = 6 - d # чередование прироста 2 и 4: 5 + 2, 7 + 4, 11 + 2, и т.д.
+    return prime
 
 def filter_numbers(numbers_list, filter_type):
     """
@@ -29,6 +38,7 @@ def filter_numbers(numbers_list, filter_type):
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
+    >>> filter_numbers([2, 3, 4, 5, 6, 7,], PRIME
     """
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
