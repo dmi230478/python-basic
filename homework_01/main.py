10908d36b5c27f7e8085e8512763deb439e0bbc2
dmi230478 <dmi230478@yandex.ru>
2022-08-09 14:56:50 +0300

    add prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -8,6 +8,7 @@ def power_numbers(* numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 3, 4, 5)
 
     <<< [1, 4, 25, 49]
     """
@@ -36,17 +37,18 @@ def filter_numbers(numbers_list, filter_type):
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
 
-
+     >>> filter_numbers([2, 3, 4, 5], ODD)
     <<< [1, 3]
+     >>> filter_numbers([2, 3, 4, 5], EVEN)
        <<< [2, 4]
-
+      >>> filter_numbers([2, 3, 4, 5], PRIME)
     """
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
         return [number for number in numbers_list if number % 2 == 0]
     if filter_type == PRIME:
-        return[is_prime()]
+        return[numbers_list == is_prime()]
 
 
 
