9def05f9dd95790004e612a96329a427322d4852
dmi230478 <dmi230478@yandex.ru>
2022-08-09 14:11:18 +0300

    add prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -19,13 +19,13 @@ EVEN = "even"
 PRIME = "prime"
 def is_prime(num):
     prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
-    i = 5;
-    d = 2;
+    i = 5
+    d = 2
 
     while prime and i * i <= num:
         prime = num % i != 0
         i += d
-        d = 6 - d # чередование прироста 2 и 4: 5 + 2, 7 + 4, 11 + 2, и т.д.
+        d = 6 - d  # чередование прироста 2 и 4: 5 + 2, 7 + 4, 11 + 2, и т.д.
     return prime
 
 def filter_numbers(numbers_list, filter_type):
